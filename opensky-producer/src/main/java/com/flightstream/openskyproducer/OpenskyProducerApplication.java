3c0db893bd2cfd55e4b0349e1517664f0970db73
Abdullah Tariq <abdullahtariq096@gmail.com>
2026-04-30 21:44:58 -0700

    feat: Setting up api for open sky

---
+++ opensky-producer/src/main/java/com/flightstream/openskyproducer/OpenskyProducerApplication.java
@@ -0,0 +1,13 @@
+package com.flightstream.openskyproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenskyProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenskyProducerApplication.class, args);
+    }
+
+}
---
+++ opensky-producer/src/test/java/com/flightstream/openskyproducer/OpenskyProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightstream.openskyproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenskyProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
